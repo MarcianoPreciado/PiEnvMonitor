555ae86353efd3d577bd1c80eb09387e1c836805
Marciano C Preciado <marcianocpreciado@gmail.com>
2020-02-22 13:16:39 -0700

    Add multi client support
    copied from realpython/materials/python-sockets-tutorial/muliconn-server.py

--- EnvMonitorMaster.py
+++ EnvMonitorMaster.py
@@ -1,22 +1,62 @@
 #!/usr/bin/env/python3
 
+import sys
 import socket
+import selectors 
+import types
+
+sel = selectors.DefaultSelector()
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print('accepted connection from',addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'b', outb=b'b')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
 
 configFile = open('NetConfig.txt', 'r')
 settings = configFile.readlines()
 HOST = settings[0][:-1]
 PORT = int(settings[1])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST,PORT))
+lsock.listen()
+print('listening on', (HOST,PORT))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+
+try:
     while True:
-        conn, addr = s.accept()
-        with conn: 
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-                print(data)
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt")
+finally:
+    sel.close()
