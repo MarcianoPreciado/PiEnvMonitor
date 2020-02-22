590114af701d095534aca030b666723ab0daf079
Marciano C Preciado <marcianocpreciado@gmail.com>
2020-02-22 11:41:20 -0700

    Add setup script
    Must be run with sudo. Also run update + upgrade with apt first

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#1/bin/bash
+
+pip3 install --upgrade setuptools
+pip3 install RPI.GPIO
+pip3 install adafruit-blinka
+pip3 install adafruit-circuitpython-mcp9808
+pip3 install adafruit-circuitpython-veml7700
