92a5b3e609c6326de363b0af4c2c0e6af23b014f
Marciano C Preciado <marcianocpreciado@gmail.com>
2020-02-22 10:57:13 -0700

    Initial Commit
    Programs for measurement and reception through TCP

---
+++ EnvMonitorRemote.py
@@ -0,0 +1,30 @@
+import time
+import board
+import busio
+import adafruit_mcp9808
+import adafruit_veml7700
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+mcp9808 = adafruit_mcp9808.MCP9808(i2c_bus)
+veml7700 = adafruit_veml7700.VEML7700(i2c_bus)
+
+
+while True:
+    tempC = mcp9808.temperature
+    tempF = tempC * 9 / 5 + 32
+    light = veml7700.light
+    lux = veml7700.lux
+    dataString = '{0:d},{1:.2f},{2:.2f},{3:.2f},{4:.2f}'.format(int(time.time()), tempC, tempF, light,lux)
+
+    dataStringBytes = dataString.encode("ascii")
+    print(dataStringBytes)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(dataStringBytes)
+        data = s.recv(1024) # TODO is this timeout reasonable
+    # TODO something about this while loop
+    time.sleep(5)
