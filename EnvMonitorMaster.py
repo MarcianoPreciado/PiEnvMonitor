439721dac27151033bc0079ff9d6cde1b00e2d68
Marciano Preciado <marcianocpreciado@gmail.com>
2020-02-23 01:43:07 -0700

    Add data logging to master

--- EnvMonitorMaster.py
+++ EnvMonitorMaster.py
@@ -7,10 +7,46 @@ import types
 
 sel = selectors.DefaultSelector()
 
+def logData(time, label, data):
+    logString = '%11d'%time
+    for item in data:
+        logString = logString + ',{}'.format(item)
+    print(logData)
+    
+    # Write to file for remote data from this source
+    filename = label + '.csv'
+    f = open(filename, a+)
+    f.write(logString)
+    f.close()
+    # TODO log to net file that data was saved
+
+def getCommas(string):
+    indicies = []
+    remaining = string
+    i = 0
+    while ',' in remaining:
+        indicies[i] = remaining.index(',')
+        remaining = remaining[indicies[i]+1:]
+        i = i + 1
+    return indicies
+
+def parsePacket(data):
+    msg = str(data.outb)
+    inds = getCommas(msg)
+    timestamp = msg[:inds[0]]
+    label     = msg[inds[0]+1:inds[1]]
+    tempC     = msg[inds[1]+1:inds[2]]
+    tempF     = msg[inds[2]+1:inds[3]]
+    ambient   = msg[inds[3]+1:inds[4]]
+    lux       = msg[inds[4]+1:]
+    
+    data = (tempC,tempf,ambient,lux)
+    logData(timestamp, label, data)
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()
-    print('accepted connection from',addr)
+    # TODO log to master net log
+    # print('accepted connection from',addr)
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b'b', outb=b'b')
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
@@ -25,12 +61,14 @@ def service_connection(key, mask):
         if recv_data:
             data.outb += recv_data
         else:
-            print('closing connection to', data.addr)
+            # TODO log to master net log
+            # print('closing connection to', data.addr)
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
+            parsePacket(data)
+            # print('echoing', repr(data.outb), 'to', data.addr)
             sent = sock.send(data.outb)
             data.outb = data.outb[sent:]
 
